f161c4388f6747d645930fb5414860f7f3e91bdb
walktorock <walktorock@gmail.com>
2018-10-07 22:45:56 +0400

    Выполнил задачу 1

--- task_1.py
+++ task_1.py
@@ -23,8 +23,8 @@ def count(num):
 #100 loops, best of 5: 543 usec per loop
 #100 loops, best of 5: 565 usec per loop
 
-cProfile.run('count("1234567")')
-#      ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#cProfile.run('count("1234567")')
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
 #         1    0.000    0.000    0.000    0.000 task_1.py:7(count)
 #         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
@@ -33,3 +33,31 @@ cProfile.run('count("1234567")')
 #         7    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
+#4. Найти сумму n элементов следующего ряда чисел: 1, -0.5, 0.25, -0.125, ...
+# Количество элементов (n) вводится с клавиатуры.
+
+
+def chain(n):
+    list = []
+    i = 0
+    z = 1
+    while i < n:
+        list.append(z)
+        z = (z * (-0.5))
+        i += 1
+    print(list)
+    b = sum(list)
+    print(b)
+
+#100 loops, best of 5: 6.98 msec per loop
+#100 loops, best of 5: 6.97 msec per loop
+#cProfile.run('chain(1000)')
+
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.005    0.005 <string>:1(<module>)
+#         1    0.000    0.000    0.005    0.005 task_1.py:40(chain)
+#         1    0.000    0.000    0.005    0.005 {built-in method builtins.exec}
+#         2    0.005    0.003    0.005    0.003 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.sum}
+#      1000    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
