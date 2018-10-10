274fb91f01479373a6c55be102746158d9ba5976
walktorock <walktorock@gmail.com>
2018-10-10 19:36:14 +0400

    Кажется работает вывод среднего дохода каждой организации

--- Задача 1.py
+++ Задача 1.py
@@ -18,14 +18,17 @@ from  collections import Counter, defaultdict, namedtuple
 # print(incomes)
 kol = int(input('Введите количество предприятий: '))
 orgs = []
-New_Org = namedtuple('New_Org', 'name income_1q income_2q income_3q income_4q')
+New_Org = namedtuple('New_Org', 'name in_1q in_2q in_3q in_4q')
 for i in range(1, kol + 1):
     name = input('Название')
-    income_1q = input('Введите прибыль за 1 квартал')
-    income_2q = input('Введите прибыль за 2 квартал')
-    income_3q = input('Введите прибыль за 3 квартал')
-    income_4q = input('Введите прибыль за 4 квартал')
-    org_1 = New_Org(name,income_1q, income_2q, income_3q, income_4q)
+    in_1q = int(input('Введите прибыль за 1 квартал: '))
+    in_2q = int(input('Введите прибыль за 2 квартал: '))
+    in_3q = int(input('Введите прибыль за 3 квартал: '))
+    in_4q = int(input('Введите прибыль за 4 квартал: '))
+    org_1 = New_Org(name,in_1q, in_2q, in_3q, in_4q)
     orgs.append(org_1)
-
-print(orgs)
+n = 0
+while n < kol :
+    avg = (orgs[n].in_1q + orgs[n].in_2q + orgs[n].in_3q + orgs[n].in_4q) / 4
+    print(f'Средняя прибыли организации {orgs[n].name} = {avg} ')
+    n += 1
